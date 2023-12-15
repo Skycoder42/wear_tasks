8af7c132c138bc5ba979bb0f686b0d032f9de37b
Skycoder42 <Skycoder42@users.noreply.github.com>
2023-12-15 12:44:28 +0100

    add wearos support

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,20 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(
+  Widget build(BuildContext context) => MaterialApp(
         home: Scaffold(
-          body: Center(
-            child: Text('Hello World!'),
+          body: Builder(
+            builder: (context) => Center(
+              child: TextButton.icon(
+                icon: const Icon(Icons.abc),
+                label: const Text('Hello World!'),
+                onPressed: () => ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Center(child: Text('Button pressed')),
+                  ),
+                ),
+              ),
+            ),
           ),
         ),
       );
