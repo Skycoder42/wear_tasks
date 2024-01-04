f42bc3aa6b9b600b8fc46d6ecaf70bb3dd1139dc
Skycoder42 <Skycoder42@users.noreply.github.com>
2024-01-05 00:22:26 +0100

    API

--- lib/src/etebase_sync/storage/collection_storage.dart
+++ lib/src/etebase_sync/storage/collection_storage.dart
@@ -1,11 +1,13 @@
 import 'package:etebase_flutter/etebase_flutter.dart';
 import 'package:hive/hive.dart';
+import 'package:logging/logging.dart';
 
 import '../hive/model/stored_collection.dart';
 
 class CollectionStorage {
   final EtebaseCollectionManager collectionManager;
   final LazyBox<StoredCollection> cacheBox;
+  final _logger = Logger('CollectionStorage');
 
   CollectionStorage({
     required this.collectionManager,
@@ -25,25 +27,31 @@ class CollectionStorage {
     bool pendingUpload = false,
   }) async {
     final uid = uidHint ?? await collection.getUid();
-    final collectionData =
-        await collectionManager.cacheSaveWithContent(collection);
+    _logger.finest(
+      'Saving collection $uid to storage (pendingUpload: $pendingUpload)',
+    );
+
     await cacheBox.put(
       uid,
       StoredCollection(
-        collectionData,
+        await collectionManager.cacheSaveWithContent(collection),
         pendingUpload: pendingUpload,
       ),
     );
+    _logger.finer('Saved collection $uid to storage');
   }
 
   Future<void> remove(String uid) async {
+    _logger.finer('Removing collection $uid from storage');
     await cacheBox.delete(uid);
   }
 
   Future<void> clear({bool fromDisk = false}) async {
     if (fromDisk) {
+      _logger.fine('Deleting collection storage from disk');
       await cacheBox.deleteFromDisk();
     } else {
+      _logger.fine('Clearing collection storage');
       await cacheBox.clear();
     }
   }
@@ -57,18 +65,33 @@ class CollectionStorage {
     String uid, {
     bool pendingUpdateOnly = false,
   }) async {
+    _logger.finest(
+      'Attempting to load collection $uid from storage '
+      '(pendingUpdateOnly: $pendingUpdateOnly)',
+    );
     final collection = await cacheBox.get(uid);
-    if (collection == null ||
-        (pendingUpdateOnly && !collection.pendingUpload)) {
+
+    if (collection == null) {
+      _logger.fine('No collection with uid $uid in storage');
+      return null;
+    } else if (pendingUpdateOnly && !collection.pendingUpload) {
+      _logger.finer('Skipping collection $uid as it is not pending');
       return null;
     }
 
+    _logger.finest('Found collection $uid');
     return collectionManager.cacheLoad(collection.data);
   }
 
   Stream<EtebaseCollection> _loadAll({
     bool pendingUpdateOnly = false,
   }) async* {
+    _logger.finest(
+      'List all collections '
+      '${pendingUpdateOnly ? 'with pending uploads ' : ''}'
+      'from storage',
+    );
+
     for (final uid in cacheBox.keys.cast<String>()) {
       final collection = await _load(uid, pendingUpdateOnly: pendingUpdateOnly);
       if (collection != null) {
