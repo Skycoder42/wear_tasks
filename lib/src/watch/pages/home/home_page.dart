908b895d039772ae5ad9ca8628ea13ece7c65427
Skycoder42 <Skycoder42@users.noreply.github.com>
2024-01-06 11:56:22 +0100

    WIP

--- lib/src/watch/pages/home/home_page.dart
+++ lib/src/watch/pages/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../../common/extensions/riverpod_extensions.dart';
 import '../../../common/localization/localization.dart';
 import '../../app/router/watch_router.dart';
 import '../../widgets/watch_scaffold.dart';
@@ -11,33 +12,37 @@ class HomePage extends HookConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) => WatchScaffold(
-        horizontalSafeArea: true,
-        loadingOverlayActive:
-            ref.watch(retryUploadsServiceProvider.select(_isLoading)),
-        body: ListView(
-          children: [
-            Hero(
-              tag: CreateTaskPage.createButtonHeroTag,
-              child: FilledButton.icon(
-                icon: const Icon(Icons.add),
-                label: Text(context.strings.home_page_create_task),
-                onPressed: () => const CreateTaskRoute().go(context),
-              ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listenForErrors(context, retryUploadsServiceProvider);
+
+    return WatchScaffold(
+      horizontalSafeArea: true,
+      loadingOverlayActive:
+          ref.watch(retryUploadsServiceProvider.select(_isLoading)),
+      body: ListView(
+        children: [
+          Hero(
+            tag: CreateTaskPage.createButtonHeroTag,
+            child: FilledButton.icon(
+              icon: const Icon(Icons.add),
+              label: Text(context.strings.home_page_create_task),
+              onPressed: () => const CreateTaskRoute().go(context),
             ),
-            OutlinedButton.icon(
-              icon: const Icon(Icons.upload),
-              label: Text(context.strings.home_page_retry_uploads),
-              onPressed:
-                  ref.watch(retryUploadsServiceProvider.select(_hasPending))
-                      ? () async => ref
-                          .read(retryUploadsServiceProvider.notifier)
-                          .uploadPending()
-                      : null,
-            ),
-          ],
-        ),
-      );
+          ),
+          OutlinedButton.icon(
+            icon: const Icon(Icons.upload),
+            label: Text(context.strings.home_page_retry_uploads),
+            onPressed:
+                ref.watch(retryUploadsServiceProvider.select(_hasPending))
+                    ? () async => ref
+                        .read(retryUploadsServiceProvider.notifier)
+                        .uploadPending()
+                    : null,
+          ),
+        ],
+      ),
+    );
+  }
 
   bool _isLoading(AsyncValue<RetryUploadsState> value) => switch (value) {
         AsyncData(value: UploadingRetryState()) => true,
