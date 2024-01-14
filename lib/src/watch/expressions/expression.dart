53ffcae57e96d05bad37dc1c6053299f2769b232
Skycoder42 <Skycoder42@users.noreply.github.com>
2024-01-14 17:04:08 +0100

    ....

--- lib/src/watch/expressions/expression.dart
+++ lib/src/watch/expressions/expression.dart
@@ -1,11 +1,25 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../../common/extensions/core_extensions.dart';
 import '../../common/localization/localization.dart';
 import '../models/task_recurrence.dart';
 import 'relative_date.dart';
 
 abstract base class Expression {
+  const Expression();
+
   String description(AppLocalizations strings);
 
   RelativeDate get relativeDate;
 
+  bool get applyDefaultTime;
+
   TaskRecurrence? get recurrence => null;
+
+  @nonVirtual
+  DateTime apply(DateTime dateTime, TimeOfDay defaultTime) {
+    final newDateTime = relativeDate.apply(dateTime);
+    return applyDefaultTime ? defaultTime.toDateTime(newDateTime) : newDateTime;
+  }
 }
--- lib/src/watch/expressions/static_expressions.dart
+++ lib/src/watch/expressions/static_expressions.dart
@@ -2,19 +2,77 @@ import '../../common/localization/localization.dart';
 import 'expression.dart';
 import 'relative_date.dart';
 
+const staticExpressions = [
+  In1Hour(),
+  ThisEvening(),
+  Tomorrow(),
+  NextWeek(),
+  NextMonth(),
+];
+
+final class In1Hour extends Expression {
+  const In1Hour();
+
+  @override
+  String description(AppLocalizations strings) => strings.expression_in_1_hour;
+
+  @override
+  bool get applyDefaultTime => false;
+
+  @override
+  RelativeDate get relativeDate => const RelativeHours(1);
+}
+
+final class ThisEvening extends Expression {
+  const ThisEvening();
+
+  @override
+  String description(AppLocalizations strings) =>
+      strings.expression_this_evening;
+
+  @override
+  bool get applyDefaultTime => false;
+
+  @override
+  RelativeDate get relativeDate => const RelativeDays.hour(0, 18, 0);
+}
+
 final class Tomorrow extends Expression {
+  const Tomorrow();
+
   @override
   String description(AppLocalizations strings) => strings.expression_tomorrow;
+
+  @override
+  bool get applyDefaultTime => true;
 
   @override
   RelativeDate get relativeDate => const RelativeDays(1);
 }
 
 final class NextWeek extends Expression {
+  const NextWeek();
+
   @override
   String description(AppLocalizations strings) => strings.expression_next_week;
 
   @override
+  bool get applyDefaultTime => true;
+
+  @override
   RelativeDate get relativeDate =>
       const RelativeWeeks.weekDay(1, DateTime.monday);
+}
+
+final class NextMonth extends Expression {
+  const NextMonth();
+
+  @override
+  String description(AppLocalizations strings) => strings.expression_next_month;
+
+  @override
+  bool get applyDefaultTime => true;
+
+  @override
+  RelativeDate get relativeDate => const RelativeMonths.monthDay(1, 1);
 }
