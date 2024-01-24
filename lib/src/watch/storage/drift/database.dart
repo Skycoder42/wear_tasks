9984f213ed99deb7c22e883afc50063d9e2742e4
Skycoder42 <Skycoder42@users.noreply.github.com>
2024-01-24 12:42:51 +0100

    .

--- lib/src/watch/storage/drift/database.dart
+++ lib/src/watch/storage/drift/database.dart
@@ -2,31 +2,9 @@ import 'package:drift/drift.dart';
 
 part 'database.g.dart';
 
-class StoredCollection extends Table {
-  TextColumn get id => text().unique()();
-  BlobColumn get data => blob()();
-  BoolColumn get pendingUpload =>
-      boolean().withDefault(const Constant(false))();
-
-  @override
-  Set<Column<Object>>? get primaryKey => {id};
-
-  @override
-  bool get isStrict => true;
-}
-
-class StoredItem extends Table {
-  TextColumn get id => text().unique()();
-  BlobColumn get data => blob()();
-
-  @override
-  Set<Column<Object>>? get primaryKey => {id};
-
-  @override
-  bool get isStrict => true;
-}
-
-@DriftDatabase(tables: [StoredCollection, StoredItem])
+@DriftDatabase(
+  include: {'database.drift'},
+)
 class AppDatabase extends _$AppDatabase {
   AppDatabase(super.e);
 
